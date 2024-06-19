dfb16cc5710088aa086eea92c43b95da26cced7a
Arun George <11051042+aruniverse@users.noreply.github.com>
2024-06-18 23:24:26 -0400

    add some basic infra (#1)
    * add some basic infra
    
    * stub ci
    
    * play around with vitest
    
    * use gh reporter for biome in ci
    
    ---------
    
    Co-authored-by: Arun George <aruniverse@users.noreply.github.com>

---
+++ src/vitest.test.ts
@@ -0,0 +1,10 @@
+import { describe, it } from "vitest";
+
+describe("vitest", () => {
+  it("should pass", () => {
+    console.log("Test passed");
+  });
+});
+
+// An entry will be shown in the report for this suite
+describe.todo('unimplemented suite')
